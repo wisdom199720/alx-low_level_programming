5ac5c1feb613d8696a4558c0bf4df943907d6cc2
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-08 14:25:16 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,7 +9,6 @@
 
 int main(void)
 {
-	char word [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(word);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
