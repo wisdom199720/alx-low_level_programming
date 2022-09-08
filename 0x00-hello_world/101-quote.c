1dbeffefe2696d1448b59675be6f4f2552b2affb
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-08 14:01:13 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	putchar(ch);
 	return (1);
 }
