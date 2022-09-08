6dc6e2557d9517a9407ca4afb7a735d88fbfabac
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-08 12:40:25 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-/**
-* main - entrypoint
-*
-* Description - using print function
-*
-* Return - always 0 (Success)
-*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
