1334c215c8b746a85baaaf07d4e262605c922404
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-08 14:13:42 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char ch [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(ch);
+	char word [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	putchar(word);
 	return (1);
 }
