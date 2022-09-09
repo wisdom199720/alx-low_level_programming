9631937411891df737911954b74f7f22b096462a
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-09 14:03:51 -0700

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, print 00 to 99 using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tenth;
+	int unit;
+
+	for (tenth = '0'; tenth <='9'; tenth++)/*print tens place*/
+	{
+		for (unit = '0'; unit <='9'; unit++)/*print units place*/
+		{
+			putchar(tenth);
+			putchar(unit);
+
+			if (!(unit == '9') && (tenth == '9'))
+			{	
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hund;
+	int ten;
+	int unit;
+
+	for (hund = '0'; hund <= '9'; hund++)
+	{
+		for ( ten ='0'; ten <= '0'; ten++)
+		{
+			for ( unit ='0'; unit <= '0'; unit++)
