a33d10550d894f38116b7e742907952cb32d1328
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-09 12:57:00 -0700

    corrections

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char alpha = 'a';
 
-	while (alpha >= 'z')
+	while (alpha <= 'z')
 	{
 		if ((alpha != 'e') && (alpha != 'q'))
 		{
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 
-	int n = 0;
+	int n = '0';
 
-	while (n < 10)
+	while (n < '10')
 	{
 		putchar(n);
 		n++;
 	}
 
-	putchar('\n');
+	putchar ('\n');
 
 	return(0);
 }
