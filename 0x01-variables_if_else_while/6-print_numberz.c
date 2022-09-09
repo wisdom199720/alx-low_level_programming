0bd4edff8fb639f08e917763137aa06a42ff1b11
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-09 12:16:24 -0700

    question 5& 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - using putchar to print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar("\n");
+
+	return(0);
+}
