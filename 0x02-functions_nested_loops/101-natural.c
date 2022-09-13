deb450834f541b09ec0ace15c8ab386113468f8f
wisdom199720 <agboighalewisdom@gmail.com>
2022-09-13 09:04:03 -0700

    advanced queesttion

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,14 +11,14 @@ int main(void)
 	int sum;
 	
 	sum = 0;
-	for (n =; n < 1024; n++)
+	for (n =0; n < 1024; n++)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
 		{
 			sum += n;
 		}
 	}
-	print("%d\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
