e6ed6874b3d3567e7e5dd4f8fea6851766d5863c
wisdom199720 <agboighalewisdom@gmail.com>
2022-10-13 12:56:14 -0700

    corrections 1 and 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(valist, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list valist;
 
 	va_start(valist, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
